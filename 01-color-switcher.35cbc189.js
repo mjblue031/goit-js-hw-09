a12cea16ebb0514634600072fbcb5f705c1386d6
mjblue031 <136666858+mjblue031@users.noreply.github.com>
2023-12-15 10:24:43 +0000

    Deploying to gh-pages from @ mjblue031/goit-js-hw-09@c3017c2fa3fa4af4193f6d3c202ec4157993b3ad 🚀

---
+++ 01-color-switcher.35cbc189.js
@@ -0,0 +1,2 @@
+let t=document.getElementById("startbutton"),e=document.getElementById("stopbutton"),d="";t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.35cbc189.js.map
